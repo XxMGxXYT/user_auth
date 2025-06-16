79ba0f0319bcb1d4a0dd45cfe1252b7095babaec
XxMGxXYT <103595155+XxMGxXYT@users.noreply.github.com>
2025-06-16 16:58:00 +0300

    Update allowedOrigins.js

--- configs/allowedOrigins.js
+++ configs/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     'http://localhost:3500',
     'https://www.google.com',
     'https://www.mywebsite.com'
+    'https://userauth-production-a7bb.up.railway.app'
 ];
 
 module.exports = allowedOrigins;
